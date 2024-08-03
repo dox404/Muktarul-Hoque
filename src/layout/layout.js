5eb901eb8400f055f45282ce2763a99a97828e10
dox404 <muktarul.hoque@hotmail.com>
2024-08-03 23:36:28 +0530

    cheking the connectivity with the repo

--- src/layout/layout.js
+++ src/layout/layout.js
@@ -13,7 +13,7 @@ function layout() {
                         <div className='flex h-screen justify-center items-center grid grid-cols-1 gap-0'>
                             <img src={require("../assets/Img/Mani.jpg")} className='h-40 rounded-full  border-4 border-rose-500 block lg:hidden xl:hidden 2xl:hidden m-auto' alt="person" />
                             <div className="pb-10">
-                                <Routes />
+                            <Routes />
                             </div>
                         </div>
 
